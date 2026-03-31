4411e1a44a2f7c54558b8a0845bb2c69fd5c24c6
Dronswa <Dronswa@gmail.com>
2026-03-31 13:34:53 +0700

    step 5

---
+++ src/games/even.js
@@ -0,0 +1,13 @@
+import runEngine from '../index.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
+
+const getGameData = () => {
+  const question = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => runEngine(description, getGameData);
