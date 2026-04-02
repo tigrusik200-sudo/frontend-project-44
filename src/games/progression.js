6bf84037f1b8c474dfd6eaf91e256b78f62bf0f0
Dronswa <Dronswa@gmail.com>
2026-04-02 13:15:05 +0700

    step 8

---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+import runEngine from '../index.js';
+import getRandomNumber from '../utils.js';
+
+const description = 'What number is missing in the progression?';
+
+const generateProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * step);
+  }
+  return progression;
+};
+
+const generateRound = () => {
+  const start = getRandomNumber(1, 20);
+  const step = getRandomNumber(2, 10);
+  const length = getRandomNumber(5, 10);
+  const hiddenIndex = getRandomNumber(0, length - 1);
+
+  const progression = generateProgression(start, step, length);
+  const correctAnswer = String(progression[hiddenIndex]);
+  
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, correctAnswer];
+};
+
+export default () => runEngine(description, generateRound);
